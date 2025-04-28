f52611f1855463dec866bbd664cdb07521370bf3
Samuele Burattini <samuele.burattini@unibo.it>
2025-04-28 22:20:32 +0200

    feat: working email form

--- themes/portio/assets/js/form-handler.js
+++ themes/portio/assets/js/form-handler.js
@@ -1,3 +1,15 @@
+function sendForm(event, form) {
+  event.preventDefault();
+  // these IDs from the previous steps
+  emailjs.sendForm('contact_service', 'contact_form', form)
+      .then(() => {
+          console.log('SUCCESS!');
+      }, (error) => {
+          console.log('FAILED...', error);
+      });
+}
+
+
 window.addEventListener("DOMContentLoaded", function () {
   var form = document.getElementById("contact-form");
   var button = document.getElementById("contact-form-button");
@@ -15,27 +27,25 @@ window.addEventListener("DOMContentLoaded", function () {
 
   // handle the form submission event
   if (form != null) {
-    form.addEventListener("submit", function (ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
+    form.addEventListener("submit", ev => sendForm(ev, form));
   }
 });
 
-// helper function for sending an AJAX request
 
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
-}
+
+// // helper function for sending an AJAX request
+
+// function ajax(method, url, data, success, error) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open(method, url);
+//   xhr.setRequestHeader("Accept", "application/json");
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+//     if (xhr.status === 200) {
+//       success(xhr.response, xhr.responseType);
+//     } else {
+//       error(xhr.status, xhr.response, xhr.responseType);
+//     }
+//   };
+//   xhr.send(data);
+// }
